5b0692c4903303f61d0dd188ebda13dc6cd6c558
Allen Dang <allengnr@gmail.com>
2012-04-30 11:46:49 +0800

    Add test data to test Chinese punctuation.

--- z2y_test.go
+++ z2y_test.go
@@ -12,15 +12,16 @@ import (
 var data = []struct {
 	origin, result string
 }{
+	{"", ""},
 	{"啊薄车单饿飞干很见空冷吗嫩哦盘群润三图玩先烟总", "abcdefghjklmnopqrstwxyz"},
 	{"中华人民共和国", "zhrmghg"},
 	{"汉语拼音都是浮云", "hypydsfy"},
-	{"世界你好！hello world!", "sjnh"},
-	{"all english", ""},
-	{"1234890", ""},
-	{"!@#$%^&*()~,.<>/?';:[]{}", ""},
-	{"转换原理是把UTF8字符先转换成gbk然后查表", "zhylsbzfxzhcrhcb"},
-	{"这@#$%^&*()~,.<>/?';:[]{}是567一abcdefghijklmnopqrst句变ABCDEFGHIJKLMNOPQRST态的测试", "zsyjbtdcs"},
+	{"世界你好！hello world!", "sjnh！hello world!"},
+	{"！，。；……（）你好￥：、——", "！，。；……（）nh￥：、——"},
+	{"all english", "all english"},
+	{"1234567890", "1234567890"},
+	{"!@#$%^&*()~,.<>/?';:[]{}", "!@#$%^&*()~,.<>/?';:[]{}"},
+	{"转换原理是把UTF8字符先转换成gbk然后查表", "zhylsbUTF8zfxzhcgbkrhcb"},
 }
 
 func TestConvertToPY(t *testing.T) {
